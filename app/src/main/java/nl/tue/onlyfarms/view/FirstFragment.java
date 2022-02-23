ac698bc5724520c3f194a94ae4a70bd14eb1e523
Zwamdurkel <github.a@riswick.net>
2022-02-23 18:39:24 +0100

    Added Homepage. Recycler/Search WIP

--- app/src/main/java/nl/tue/onlyfarms/view/FirstFragment.java
+++ app/src/main/java/nl/tue/onlyfarms/view/FirstFragment.java
@@ -37,6 +37,14 @@ public class FirstFragment extends Fragment {
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
+
+        binding.buttonHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_homeView);
+            }
+        });
     }
 
     @Override
