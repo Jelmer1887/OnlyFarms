ae9e9663c1466c828eeb64e9d2dc55dffa80f8b5
Jelmer <j.schuttert@student.tue.nl>
2022-03-09 00:29:34 +0100

    Re-created UI + Fixed bugs related to registration activity
    - re-created the UI originally created for the registration screen}
    + still has some errors and warning
    - adjusted UI to scale and adjust properly to narrower screen aspect ratios
    
    - fixed being able to go back on registration screen to exit app
    - fixed being able to go back to registration screen after registering
    - fixed being able to go back to registration screen when already logged in
    - added toast to notify that the user is logged in
    - removed action-bar from login-activity
    
    - added deployment.xml related file to gitnore, it was generated when launching on a VM

--- app/src/main/java/nl/tue/onlyfarms/view/RegisterView.java
+++ app/src/main/java/nl/tue/onlyfarms/view/RegisterView.java
@@ -6,9 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -20,8 +22,9 @@ import nl.tue.onlyfarms.R;
 import nl.tue.onlyfarms.viewmodel.RegisterViewModel;
 
 public class RegisterView extends AppCompatActivity {
-    EditText eMailElement, passwordElement;
+    EditText firstNameElement, lastNameElement, eMailElement, passwordElement;
     Button confirmButtonElement;
+    RadioButton accountTypeClientElement, accountTypeVendorElement;
     ProgressBar spinnerElement;
     FirebaseAuth firebaseAuth;
 
@@ -29,45 +32,56 @@ public class RegisterView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
 
         // get firebase authentication instance
         firebaseAuth = FirebaseAuth.getInstance();
 
         // check if user is already logged in
         if (firebaseAuth.getCurrentUser() != null){
+            Toast.makeText(RegisterView.this, "Already logged in", Toast.LENGTH_LONG).show();
             startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
         }
 
         // initialize component representations (retrieve elements from UI)
-        eMailElement = findViewById(R.id.login_field_email);
-        passwordElement = findViewById(R.id.login_field_password);
-        confirmButtonElement = findViewById(R.id.register_button_submit);
-        spinnerElement = findViewById(R.id.register_progressBar);
+        firstNameElement = findViewById(R.id.register_firstName);
+        lastNameElement = findViewById(R.id.register_lastName);
+        accountTypeClientElement = findViewById(R.id.register_accountType_client);
+        accountTypeVendorElement = findViewById(R.id.register_accountType_vendor);
+        eMailElement = findViewById(R.id.register_email);
+        passwordElement = findViewById(R.id.register_password);
+        confirmButtonElement = findViewById(R.id.register_submit);
+        spinnerElement = findViewById(R.id.register_spinner);
 
         confirmButtonElement.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 // --- check the data filled in by the user
-                if (!RegisterViewModel.checkEmail(eMailElement) ||
-                        !RegisterViewModel.checkPassword(passwordElement)){
+
+                if (!RegisterViewModel.checkFields(new EditText[]{firstNameElement, lastNameElement, eMailElement, passwordElement}, passwordElement)){
+                    spinnerElement.setVisibility(View.INVISIBLE);
                     return;
                 }
-
-                String email = eMailElement.getText().toString().trim();
-                String password = passwordElement.getText().toString().trim();
 
                 // --- register the user with the database
                 spinnerElement.setVisibility(View.VISIBLE);
 
                 // add user to firebase, display error to user if failed or redirected is success.
-                firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                firebaseAuth.createUserWithEmailAndPassword(
+                        eMailElement.getText().toString().trim(),
+                        passwordElement.getText().toString().trim())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()){
+                            spinnerElement.setVisibility(View.INVISIBLE);
                             Toast.makeText(RegisterView.this, "Account created!", Toast.LENGTH_LONG).show();
                             startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            finish();
                         } else {
+                            spinnerElement.setVisibility(View.INVISIBLE);
                             Toast.makeText(RegisterView.this, "Something went wrong! " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
                         }
                     }
--- app/src/main/java/nl/tue/onlyfarms/viewmodel/RegisterViewModel.java
+++ app/src/main/java/nl/tue/onlyfarms/viewmodel/RegisterViewModel.java
@@ -1,5 +1,6 @@
 package nl.tue.onlyfarms.viewmodel;
 
+import android.graphics.Color;
 import android.text.TextUtils;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -8,6 +9,27 @@ public class RegisterViewModel {
 
     private static boolean checkEmpty(String val){
         return TextUtils.isEmpty(val);
+    }
+
+    public  static boolean checkFields(EditText[] fields, EditText pwrd){
+        boolean valid = true;
+        for (EditText f : fields) {
+            String fieldValue = f.getText().toString().trim();
+            if (!checkEmpty(fieldValue)) {
+                continue;   // guard: skip non-empty fields
+            }
+            f.setText("");
+            f.setError(f.getHint() + " must be filled in!");
+            valid = false;
+        }
+
+        if (pwrd.getText().toString().trim().length() < 6) {
+            pwrd.setText("");
+            pwrd.setError(pwrd.getHint() + " must be at least 6 characters long!");
+            valid = false;
+        }
+
+        return valid;
     }
 
     public static boolean checkEmail(EditText email){
