0b3b84326f6bceb1d2395fdd41390c490644fcbb
Saibot <tobyo@live.nl>
2022-03-12 16:45:53 +0100

    Added some way to retrieve elements, but I don't have time to test it or to figure out what all the overrides do just yet...
    for more info, see https://firebase.google.com/docs/database/admin/retrieve-data

--- app/src/main/java/nl/tue/onlyfarms/model/FirebaseUserService.java
+++ app/src/main/java/nl/tue/onlyfarms/model/FirebaseUserService.java
@@ -1,6 +1,14 @@
 package nl.tue.onlyfarms.model;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class FirebaseUserService {
 
@@ -8,4 +16,38 @@ public class FirebaseUserService {
         FirebaseDatabase database = OurFirebaseDatabase.getInstance();
         database.getReference().child("users").child(user.getUid()).setValue(user);
     }
+
+    // function that will hopefully find a user by name (this should work, but I don't have time to test it...
+    public static void getUser() {
+        final FirebaseDatabase database = OurFirebaseDatabase.getInstance();
+        final DatabaseReference userRef = database.getReference("users");
+        userRef.orderByChild("firstName").equalTo("Tobias").addChildEventListener(new ChildEventListener() {
+
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                System.out.println(snapshot.getKey());
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        }
 }
