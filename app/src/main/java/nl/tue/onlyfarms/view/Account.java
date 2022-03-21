8672fca74e752886f464c9895a0e41cc316b902d
Jelmer <j.schuttert@student.tue.nl>
2022-03-22 00:25:04 +0100

    added viewModel to account + updated id's

--- app/src/main/java/nl/tue/onlyfarms/view/Account.java
+++ app/src/main/java/nl/tue/onlyfarms/view/Account.java
@@ -4,15 +4,22 @@ package nl.tue.onlyfarms.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import nl.tue.onlyfarms.R;
+import nl.tue.onlyfarms.viewmodel.HomeViewModel;
 
 public class Account extends Fragment {
+
+    private static final String TAG = "AccountFragment";
+    private HomeViewModel model;
 
     public static Account newInstance() {
         return new Account();
@@ -23,5 +30,11 @@ public class Account extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_account, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        model = new ViewModelProvider(requireActivity()).get(HomeViewModel.class);
     }
 }
