465c782929f65a65ffdb1dd64a7aec01c59e865f
Zwamdurkel <github.a@riswick.net>
2022-03-22 21:25:24 +0100

    Added store detail page

--- app/src/main/java/nl/tue/onlyfarms/view/StoreGeneral.java
+++ app/src/main/java/nl/tue/onlyfarms/view/StoreGeneral.java
@@ -2,7 +2,9 @@ package nl.tue.onlyfarms.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import nl.tue.onlyfarms.R;
 
@@ -16,5 +18,12 @@ public class StoreGeneral extends AppCompatActivity {
         // Enabled action bar back to Base activity
         setSupportActionBar(findViewById(R.id.topBar));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        findViewById(R.id.see_more).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), StoreDetail.class));
+            }
+        });
     }
 }
