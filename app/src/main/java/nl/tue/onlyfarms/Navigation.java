46ad291e6c4d20c087e2042809574dca38abcd87
aloys <github.a@riswick.net>
2022-03-23 13:06:01 +0100

    Changed nav logic to work with client/vendor

--- app/src/main/java/nl/tue/onlyfarms/Navigation.java
+++ app/src/main/java/nl/tue/onlyfarms/Navigation.java
@@ -4,40 +4,46 @@ import androidx.fragment.app.Fragment;
 
 import java.util.Map;
 import java.util.HashMap;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 import nl.tue.onlyfarms.view.Account;
+import nl.tue.onlyfarms.view.Settings;
 import nl.tue.onlyfarms.view.addStore;
 import nl.tue.onlyfarms.view.client.Home;
+import nl.tue.onlyfarms.view.vendor.HomeVendor;
 
 public class Navigation {
     private static Navigation instance;
     // Supplier to make sure we get a new instance every time.
-    private static Map<Integer, Supplier<Fragment>> navTargets = new HashMap<>();
+    private static Map<Integer, Function<Boolean, Fragment>> navTargets = new HashMap<>();
 
-    public Navigation(){}
+    public Navigation() {}
 
     public static Navigation getInstance() {
         if (instance == null) {
             instance = new Navigation();
-            instance.addNavElement(R.id.navto_home, Home::new);
-            instance.addNavElement(R.id.navto_mystore, addStore::new);
-            instance.addNavElement(R.id.navto_myaccount, Account::new);
-            instance.addNavElement(R.id.navto_reservations, Home::new);
+            instance.addNavElement(R.id.navto_home, (isClient) -> {return isClient ? new Home() : new HomeVendor();
+            });
+            instance.addNavElement(R.id.navto_mystore, (isClient) -> {return new addStore();});
+            instance.addNavElement(R.id.navto_myaccount, (isClient) -> {return new Account();});
+            instance.addNavElement(R.id.navto_reservations, (isClient) -> {return new Home();});
+            instance.addNavElement(R.id.navto_settings, (isClient) -> {return new Settings();});
         }
 
         return instance;
     }
 
-    public void addNavElement(int id, Supplier<Fragment> destination){
+    public void addNavElement(int id, Function<Boolean, Fragment> destination){
         navTargets.put(id, destination);
     }
 
-    public Fragment toNavigator(int id) {
+    public Fragment toNavigator(int id, boolean isClient) {
         if (!navTargets.containsKey(id)) {
             throw new IllegalArgumentException("requested ID not present in nav");
         }
 
-        return navTargets.get(id).get();
+        return navTargets.get(id).apply(isClient);
     }
+
 }
