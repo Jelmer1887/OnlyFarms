dd866af9619f5ee2e44f3e20361102eaea1552b1
Zwamdurkel <github.a@riswick.net>
2022-03-25 20:42:39 +0100

    Fixed back bug in Store

--- app/src/main/java/nl/tue/onlyfarms/view/StoreDetail.java
+++ app/src/main/java/nl/tue/onlyfarms/view/StoreDetail.java
@@ -3,6 +3,7 @@ package nl.tue.onlyfarms.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import nl.tue.onlyfarms.R;
 import nl.tue.onlyfarms.model.Store;
@@ -21,5 +22,15 @@ public class StoreDetail extends AppCompatActivity {
         // Enabled action bar back to Base activity
         setSupportActionBar(findViewById(R.id.topBar));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // close current activity and return to previous
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
