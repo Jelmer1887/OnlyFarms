21ef7f977edd911d3599c8d493af7e6341bdc66e
20203584 <i.s.veldhuizen@student.tue.nl>
2022-03-30 13:33:59 +0200

    Added java file for add product activity/

---
+++ app/src/main/java/nl/tue/onlyfarms/view/vendor/AddProduct.java
@@ -0,0 +1,16 @@
+package nl.tue.onlyfarms.view.vendor;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import nl.tue.onlyfarms.R;
+
+public class AddProduct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_addproduct);
+    }
+}
