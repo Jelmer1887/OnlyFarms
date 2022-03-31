e7029d4c1ae33584c1a6f96aa13cdd6eb01e7989
Jelmer <j.schuttert@student.tue.nl>
2022-03-31 22:53:18 +0200

    changed store UID type for generation

--- app/src/main/java/nl/tue/onlyfarms/view/addStore.java
+++ app/src/main/java/nl/tue/onlyfarms/view/addStore.java
@@ -14,6 +14,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Calendar;
 import java.util.Random;
+import java.util.UUID;
 
 import nl.tue.onlyfarms.R;
 import nl.tue.onlyfarms.model.FireBaseService;
@@ -41,7 +42,7 @@ public class addStore extends Fragment {
             String placeHolder_name = String.valueOf(r.nextLong());
             String placeHolder_desc = String.valueOf(r.nextLong());
             String placeHolder_location = String.valueOf(r.nextDouble());
-            String uid = Long.toString(Calendar.getInstance().getTime().getTime());
+            String uid = UUID.randomUUID().toString();
             String userUid = FirebaseAuth.getInstance().getCurrentUser().getUid();
             Store newStore = new Store(uid, userUid, placeHolder_name, placeHolder_desc, placeHolder_location);
             // TODO: implement viewModel!
