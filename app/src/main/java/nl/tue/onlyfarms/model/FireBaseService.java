7fc529d71b3d3404fdb5b6e31221e5ed3a1f7bd2
Jelmer <j.schuttert@student.tue.nl>
2022-04-15 10:58:15 +0200

    COMMENTS

--- app/src/main/java/nl/tue/onlyfarms/model/FireBaseService.java
+++ app/src/main/java/nl/tue/onlyfarms/model/FireBaseService.java
@@ -24,16 +24,33 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
-
+/*
+* This Generic class is used to facilitate communication with the database.
+* It aims to:
+* - Retrieve data from the database, using important fields
+* - Upload and Update data to the database
+* - Ensure received data remains up to date, through observers and listeners
+*
+* The class uses generic types to prevent duplication.
+* For every kind of uploadable Model, a user would create a service.
+* */
 public class FireBaseService<T extends Model> {
-    private final Class<T> aClass;
     private String TAG = "FireBaseService<";
 
+    // class of the type of Model to store in the database, determines as what kind of
+    // object data is retrieved and uploaded.
+    private final Class<T> aClass;
+
+    // The database connection is handles through OurFireBaseDatabase
     private final static FirebaseDatabase database = OurFirebaseDatabase.getInstance();
     private final DatabaseReference reference;
     private final String referenceString;
 
-
+    /*
+    * Constructor for a new service,
+    * @param aClass defines what kind of object is used
+    * @param ref defines where data should be retrieved from, uploaded to and observed.
+    * */
     public FireBaseService(Class<T> aClass, String ref){
         this.aClass = aClass;
         this.TAG = TAG + aClass.getName() + ">";
@@ -43,6 +60,9 @@ public class FireBaseService<T extends Model> {
         Log.d(TAG, "service created looking at " + ref);
     }
 
+    // Returns a MutableLiveData that contains a Set of the objects at the Reference
+    // will NEVER have duplicates (it's a set)
+    // the Set is updated with new changes as data in the database changes
     public MutableLiveData<Set<T>> getAllAtReference() {
         ExistanceListener e = new ExistanceListener();
         DataListener l = new DataListener(aClass,e.getExistance().getValue());
@@ -52,6 +72,10 @@ public class FireBaseService<T extends Model> {
         return l.getMultipleResults();
     }
 
+    // Returns a MutableLiveData that contains a Set of the objects at the Reference,
+    // that contain the value specified at the specified field
+    // will NEVER have duplicates (it's a set)
+    // the Set is updated with new changes as data in the database changes
     public MutableLiveData<Set<T>> getAllMatchingField(String field, String value){
         ExistanceListener e = new ExistanceListener();
         DataListener l = new DataListener(aClass, e.getExistance().getValue());
@@ -62,6 +86,10 @@ public class FireBaseService<T extends Model> {
         return l.getMultipleResults();
     }
 
+    // Returns a MutableLiveData that contains the first found object at the Reference,
+    // that contain the value specified at the specified field
+    // not guaranteed to return the same object, if multiple objects match the query.
+    // the Object is updated with new changes as data in the database changes
     public MutableLiveData<T> getSingleMatchingField(String field, String value){
         ExistanceListener e = new ExistanceListener();
         DataListener l = new DataListener(aClass, e.getExistance().getValue());
@@ -71,38 +99,60 @@ public class FireBaseService<T extends Model> {
         return l.getSingleResult();
     }
 
+    // Store a object in the database
+    // the object has to be of the type that is specified for this service.
     public Task<Void> updateToDatabase(T object, String uid) {
         Log.d(TAG, "uploading " + object + " data to " + referenceString + "/" + uid);
         return database.getReference()
                 .child(referenceString).child(uid).setValue(object);
     }
 
+    /*
+    * Delete an object in the database matching the specified uid,
+    * does nothing if no object is found
+    * returns a Task, so the user may listen-in on the deletion process, and respond by adding
+    * listeners.
+    * */
     public Task<Void> deleteFromDatabase(String uid) {
         Log.d(TAG, "deleting " + referenceString + "/"+uid + " if found");
         return database.getReference().child(referenceString)
                 .child(uid).removeValue();
     }
 
+    /*
+    * A custom ValueEventListener to match our app
+    * an existence listener is used to detect whether or not a query is not returned,
+    * this happens when no match is found.
+    */
     private class ExistanceListener implements ValueEventListener {
+        // result boolean
         private MutableLiveData<Boolean> existance = new MutableLiveData<>(false);
 
         public ExistanceListener() {}
 
+        // getter
         public MutableLiveData<Boolean> getExistance() {
             return existance;
         }
 
         @Override
+        // onDataChange of a ValueEventListener is guaranteed to return, and used to detect
+        // the existence of valid data.
         public void onDataChange(@NonNull DataSnapshot snapshot) {
             existance.postValue(snapshot.exists());
         }
 
         @Override
+        // Throw an error if the request failed
         public void onCancelled(@NonNull DatabaseError error) {
             throw error.toException();
         }
     }
 
+    /*
+    * Custom childEventListener class,
+    * responsible for keeping returned data in the app up-to-date
+    * */
     private class DataListener implements ChildEventListener{
         private static final String TAG = "DataListener";
         private final MutableLiveData<T> result;
@@ -110,12 +160,22 @@ public class FireBaseService<T extends Model> {
         private final Class<T> aClass;
         private final Set<T> multiple_found;
 
+        // Constructor used to create a new listener
         public DataListener(Class<T> aClass, Boolean existence) {
             this.multiple_found = new HashSet<>();
             this.result = new MutableLiveData<>();
             this.results = new MutableLiveData<>();
             this.aClass = aClass;
 
+            /*
+            * when a DataListener is added to a query that is empty (e.g. no valid result found),
+            * the onChildXXXXXXX methods may not be called at all, and thus single result
+            * may never be initialised properly.
+            *
+            * Thus, to check for existance, and ExistanceListener should first be added to the
+            * same query, and it's result should be passed to this listener,
+            * so the single result variable can be set correctly.
+            * */
 
             if (!existence) {
                 this.results.postValue(multiple_found);
@@ -123,6 +183,8 @@ public class FireBaseService<T extends Model> {
             }
         }
 
+        // updates the single result field
+        // (to null if none are found)
         private void determineSingleResult() {
             Iterator<T> iter = multiple_found.iterator();
             if (iter.hasNext()) {
@@ -131,6 +193,8 @@ public class FireBaseService<T extends Model> {
             }
             result.postValue(null);
         }
+
+        // -- getters --
 
         public MutableLiveData<Set<T>> getMultipleResults() {
             return results;
@@ -141,22 +205,35 @@ public class FireBaseService<T extends Model> {
         }
 
         @Override
+        // This method is called for every child found in the query, and for every child that is
+        // later added to the query.
         public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
             Log.d(TAG, "addition change detected");
+
+            // retrieve (newly added) values at the query
             T foundResult = snapshot.getValue(aClass);
             if (foundResult == null) {
-                throw new IllegalStateException("this btch empty-yo!");
+                throw new IllegalStateException("the result is null!");
             }
+
+            // update mutableLiveData objects with new values
             multiple_found.add(foundResult);
             results.postValue(multiple_found);
             determineSingleResult();
         }
 
         @Override
+        // This method is called for every child in the query, that changes (e.g. a name is changed).
         public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
             Log.d(TAG, "edit change detected");
+
+            // retrieve changed value
             T foundResult = snapshot.getValue(aClass);
             if (foundResult == null) { return; }
+
+            // update the corresponding object in the MutableLiveData:
+            // find the object by id,
+            // then remove the old one and add the new one.
             if (multiple_found.removeIf(object -> object.getUid().equals(foundResult.getUid()))) {
                 multiple_found.add(foundResult);
                 results.postValue(multiple_found);
@@ -165,27 +242,37 @@ public class FireBaseService<T extends Model> {
         }
 
         @Override
+        // This method is called for every child in the query that is removed
+        // -> was in the query and in the app, but is now removed from the database.
+        // usually triggers after deleteFromDatabase is called.
         public void onChildRemoved(@NonNull DataSnapshot snapshot) {
             Log.d(TAG, "removal change detected");
+
+            // retrieve the removed value
             T removedResult = snapshot.getValue(aClass);
             if (removedResult == null){ return; }
-            Log.d(TAG, "looking for object in lists...");
+
+            // remove the object from the MutableLiveData, by id.
             if (multiple_found.removeIf(object -> object.getUid().equals(removedResult.getUid()))) {
-                Log.d(TAG, "found!");
                 results.postValue(multiple_found);
                 determineSingleResult();
             } else {
-                Log.d(TAG, "not found!");
+                Log.d(TAG, "removed object not found!");
             }
         }
 
         @Override
+        // called when a child is moved from one place in the query to another.
+        // This doesn't change anything for the availability of the data,
+        // and is thus ignored.
         public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
 
         }
 
         @Override
+        // Called when the query fails
         public void onCancelled(@NonNull DatabaseError error) {
+            // echo the exception thrown by FireBase
             throw error.toException();
         }
     }
--- app/src/main/java/nl/tue/onlyfarms/model/User.java
+++ app/src/main/java/nl/tue/onlyfarms/model/User.java
@@ -3,21 +3,51 @@ package nl.tue.onlyfarms.model;
 import com.google.firebase.database.IgnoreExtraProperties;
 
 @IgnoreExtraProperties
+
+/*
+* User class representing a user in our system.
+* This class descripes what fields a user should have for
+* data to be stored, as well as containing
+* setters and getters for all fields.
+*
+* The class extends a general Model, which contains setters and getters for UID's
+* This is required to allow this class to be uploaded, retrieved to FireBase.
+* */
 public class User implements Model {
+    /*
+    * A user has:
+    * - a unique identifier, usually created by FireBaseAuthentication services upon registration
+    * - a userName to easily display inside the app
+    * - a First name
+    * - a Last name
+    * - an email address, which has to be properly formatted (checked by FireBase)
+    * - a status, a user is either a VENDOR or a CLIENT
+    * */
+
+    /*
+    * The status of a user, used to identify what kind of subtype a user is
+    * The user functionality changes based on whether they are:
+    * - a VENDOR, to see their stores, etc...
+    * - a CLIENT, to see offers, etc...
+    */
     public enum Status {
         VENDOR,
         CLIENT
     }
 
-    private String uid;
-    private String userName;
-    private String firstName;
-    private String lastName;
-    private String emailAddress;
-    private Status status;
+    // Fields to store relevant user data
+    private String uid;             // identifier of the user as string, used to match the authentication database data with the user database info
+    private String userName;        // short display name
+    private String firstName;       // legal first name
+    private String lastName;        // actual last name
+    private String emailAddress;    // emailAddress, should match the same field
+    private Status status;          // status, as Status defined above, either VENDOR or CLIENT
 
+    /* Obligatory empty constructor for FireBase */
     public User() {}
 
+
+    /* Actually used constructor inside the app */
     public User(String uid, String userName, String firstName, String lastName, String emailAddress, Status status) {
         this.uid = uid;
         this.userName = userName;
@@ -27,6 +57,10 @@ public class User implements Model {
         this.status = status;
     }
 
+
+    /*--- Setters and Getters ---*/
+
+    // getUid and setUid are defined here again, to overwrite and implement behaviour of Model
     public String getUid() {
         return uid;
     }
